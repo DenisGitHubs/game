bb412ae0572f997b8b3d8b96f041ef19f980e6ed
DenisGitHubs <sokurenkoda87@gmail.com>
2023-07-06 12:35:00 +0700

    3.3-2

--- index.js
+++ index.js
@@ -2,15 +2,8 @@ import { levelStr } from "./levelChoice.js";
 
 window.user = {};
 
-
-
 levelStr();
-
 
 //ограничение по времени
 // анимация карт
 // удаление карт после правильных ответов
-
-
-
-
