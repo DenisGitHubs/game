99b48c53a23c771317d341404b1b2357e7493105
DenisGitHubs <sokurenkoda87@gmail.com>
2023-07-06 10:52:12 +0700

    3.3

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import { levelStr } from "./levelChoice.js";
+
 window.user = {};
-levelStr()
+
+
+
+levelStr();
+
+
+//ограничение по времени
+// анимация карт
+// удаление карт после правильных ответов
 
 
 
--- levelChoice.js
+++ levelChoice.js
@@ -1,22 +1,24 @@
 
-import { gamePageLvl } from "./game.js";
-let levelChoice = 0;
+import { goGame } from "./game.js";
+import { randomCards, cardRandomMass } from "./random.js";
+import { allCardsMass } from "./cardsMass.js";
+export let levelChoice = 0;
 export function levelStr() {
     const levelEl = document.querySelector('body');
-const levelHtml = `
-<div class="container">
+    const levelHtml = `
+<div class="container center">
     <div class="container-text"><p>Выбери сложность</p></div>
     <div class="container-level-buttons">
-        <div><button class=${levelChoice == 0 ? "buttons-button" : levelChoice == 1 ? "choice-button" : "buttons-button"} id="1">1</button></div>
-        <div><button class=${levelChoice == 0 ? "buttons-button" : levelChoice == 2 ? "choice-button" : "buttons-button"} id="2">2</button></div>
-        <div><button class=${levelChoice == 0 ? "buttons-button" : levelChoice == 3 ? "choice-button" : "buttons-button"} id="3">3</button></div>
+        <div><button class=${levelChoice == 0 ? "buttons-button" : levelChoice == 3 ? "choice-button" : "buttons-button"} id="3">1</button></div>
+        <div><button class=${levelChoice == 0 ? "buttons-button" : levelChoice == 6 ? "choice-button" : "buttons-button"} id="6">2</button></div>
+        <div><button class=${levelChoice == 0 ? "buttons-button" : levelChoice == 9 ? "choice-button" : "buttons-button"} id="9">3</button></div>
     </div>
     <div class="container-start-button"><button class="start-button" id="start">Старт</button></div>
 </div>`
-levelEl.innerHTML = levelHtml;
+    levelEl.innerHTML = levelHtml;
 
-choiceButton(levelChoice)
-startButton(levelChoice)
+    choiceButton()
+    startButton()
 
 }
 
@@ -24,25 +26,44 @@ function choiceButton() {
     const choiceButtons = document.getElementsByClassName("buttons-button");
     for (const choiceButton of choiceButtons) {
         choiceButton.addEventListener("click", () => {
+
             levelChoice = choiceButton.id;
 
             levelStr()
         });
-            
-        
+
+
     }
 
 }
-function startButton(levelChoice) {
+function startButton() {
     document.getElementById("start").addEventListener('click', () => {
         if (levelChoice == 0) {
             alert("Выберете сложность")
             return
         }
-        window.user.level = levelChoice
 
-        console.log(window.user.level)
-        gamePageLvl()
+        cardRandomMass.splice(0, 18)
+        window.user.level = levelChoice;
+        randomCards(levelChoice, allCardsMass)
+        // gamePageLvl()
+        cardRandomMass.push.apply(cardRandomMass, cardRandomMass);
+
+        shuffle(cardRandomMass);
+        goGame(cardRandomMass, levelChoice)
     })
 
 }
+const shuffle = (cardRandomMass) => {
+    let m = cardRandomMass.length, t, i;
+
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = cardRandomMass[m];
+        cardRandomMass[m] = cardRandomMass[i];
+        cardRandomMass[i] = t;
+    }
+
+    return cardRandomMass;
+}
+
